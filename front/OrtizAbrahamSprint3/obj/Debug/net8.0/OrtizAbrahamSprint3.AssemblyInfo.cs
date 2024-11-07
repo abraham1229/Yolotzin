9303e7ec3b2f235d1c1a5a74b52562957b8c6860
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-11-07 14:43:43 -0600

    Propuesta a solucion de video de youtube, parecd funcionar

--- front/OrtizAbrahamSprint3/obj/Debug/net8.0/OrtizAbrahamSprint3.AssemblyInfo.cs
+++ front/OrtizAbrahamSprint3/obj/Debug/net8.0/OrtizAbrahamSprint3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrtizAbrahamSprint3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881f1d4bbc7096dd8ab6c92f429e21a6523c7307")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99baf5bca6da09f5f30b1cd4d9d7236335225df")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrtizAbrahamSprint3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrtizAbrahamSprint3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
