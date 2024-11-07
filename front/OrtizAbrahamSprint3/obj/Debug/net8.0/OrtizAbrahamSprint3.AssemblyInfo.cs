4c752346423fad6389bcecabdbcdc8793cfa1288
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-11-06 18:35:51 -0600

    Se corigue imagen de polkas yo con luchi en about us

--- front/OrtizAbrahamSprint3/obj/Debug/net8.0/OrtizAbrahamSprint3.AssemblyInfo.cs
+++ front/OrtizAbrahamSprint3/obj/Debug/net8.0/OrtizAbrahamSprint3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrtizAbrahamSprint3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af010a1a1a23b7c8ed6fc369142db71da0f5e83f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c9d1552dd9e09d3a78760a235c1bf34ed5c8f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrtizAbrahamSprint3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrtizAbrahamSprint3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
