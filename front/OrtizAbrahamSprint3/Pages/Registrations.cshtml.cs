8d8c89883e1de80b03aa3c10083f01bc0a03d0ee
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-11-14 14:42:18 -0600

    Se agregan logica de log in

--- front/OrtizAbrahamSprint3/Pages/Registrations.cshtml.cs
+++ front/OrtizAbrahamSprint3/Pages/Registrations.cshtml.cs
@@ -1,51 +1,66 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.RazorPages;
-    using Microsoft.AspNetCore.Mvc.Rendering;
-    using OrtizAbrahamSprint3.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OrtizAbrahamSprint3.Data;
+using OrtizAbrahamSprint3.Data.Entities;
+using System.Reflection.Metadata.Ecma335;
 
-    namespace OrtizAbrahamSprint3.Pages
+namespace OrtizAbrahamSprint3.Pages
+{
+    //[Authorize]
+    public class RegistrationsModel : PageModel
     {
-        [Authorize]
-        public class RegistrationsModel : PageModel
+        //Declare the class for every ID
+
+        [BindProperty]
+        public int AgeRangeID { get; set; }
+
+        [BindProperty]
+        public int LevelID { get; set; }
+        [BindProperty]
+        public int StyleID { get; set; }
+        [BindProperty]
+        public int UserID { get; set; }
+
+        //Declare the lists to get the data
+        public SelectList listofagerange { get; set; }
+        public SelectList listoflevels { get; set; }
+        public SelectList listofstyles { get; set; }
+
+        //Messages to send errors
+        public string MessageAgeRange { get; set; }
+        public string MessageLevel { get; set; }
+        public string MessageStyle { get; set; }
+
+        //This readonly variable needs to be the same name as your DbContext class previously created.
+        private readonly MyApplicationDbContext _myApplicationDbContext;
+
+
+        //The following code injects our Database context into our Razor Page Model.
+        public RegistrationsModel(MyApplicationDbContext myApplicationDbContext)
         {
-            //Declare the class for every ID
-            [BindProperty]
-            public int InstructorID { get; set; }
-            [BindProperty]
-            public int StyleID { get; set; }
-            [BindProperty]
-            public int LevelID { get; set; }
-            [BindProperty]
-            public int AgeRangeID { get; set; }
-            [BindProperty]
-            public int WeekDaysID { get; set; }
+            _myApplicationDbContext = myApplicationDbContext;
+        }
 
-            //Declare the lists to get the data
-            public SelectList listofinstructors { get; set; }
-            public SelectList listofstyles { get; set; }
-            public SelectList listoflevels { get; set; }
-            public SelectList listofagerange { get; set; }
-            public SelectList listofweekdays { get; set; }
+        public void OnGet()
+        {
+            //Populate the dropdownlist with all possible chair types from our database table.
+            listofagerange = new SelectList(_myApplicationDbContext.AgeRange, "AgeRangeID", "RangeName");
+            listoflevels = new SelectList(_myApplicationDbContext.Levels, "LevelID", "LevelName");
+            listofstyles = new SelectList(_myApplicationDbContext.Style, "StyleID", "StyleName");
+        }
 
-            //This readonly variable needs to be the same name as your DbContext class previously created.
-            private readonly MyApplicationDbContext _myApplicationDbContext;
-
-
-            //The following code injects our Database context into our Razor Page Model.
-            public RegistrationsModel(MyApplicationDbContext myApplicationDbContext)
+        public IActionResult OnPostEnroll()
+        {
+            if (AgeRangeID == 0)
             {
-                _myApplicationDbContext = myApplicationDbContext;
-            }
-
-            public void OnGet()
-            {
-                //Populate the dropdownlist with all possible chair types from our database table.
-                listofinstructors = new SelectList(_myApplicationDbContext.Instructor, "InstructorID", "FirstName");
-                listofstyles = new SelectList(_myApplicationDbContext.Style, "StyleID", "StyleName");
-                listoflevels = new SelectList(_myApplicationDbContext.Levels, "LevelID", "LevelName");
+                MessageAgeRange = "Please select an age range";
                 listofagerange = new SelectList(_myApplicationDbContext.AgeRange, "AgeRangeID", "RangeName");
-                listofweekdays = new SelectList(_myApplicationDbContext.WeekDays, "WeekDaysID", "WeekDaysName");
             }
+
+            return Page();
         }
     }
+        
+}
